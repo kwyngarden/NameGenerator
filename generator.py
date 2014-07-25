ea799b7a942c08fcaa7d92a066fd0f73268fe248
Keith Wyngarden <kwyngard@stanford.edu>
2014-07-25 15:45:35 -0700

    Better end handling, variable numbers of groups, and alternate dictionaries

--- generator.py
+++ generator.py
@@ -1,5 +1,6 @@
+from math import floor
 from nltk.util import bigrams
-from random import choice, sample
+from random import choice, normalvariate, random, sample
 
 VOWELS = 'aeiou'
 CONSONANTS = 'bcdfghjklmnpqrstvwxyz'
@@ -18,6 +19,12 @@ class NameGeneratorModel:
             return choice(self.start_groups_vowels)
         else:
             return choice(self.start_groups_consonants)
+
+    def get_end_group_following(self, letter):
+        next_letter = choice(self.bigram_map.get(letter))
+        group_list = self.end_groups_consonants if letter in VOWELS else self.end_groups_vowels
+        return choice([group for group in group_list if group.startswith(next_letter)])
+        
 
     def get_group_following(self, letter):
         next_letter = choice(self.bigram_map.get(letter))
@@ -54,20 +61,31 @@ class NameGeneratorModel:
             else:
                 current_group_is_vowels = not current_group_is_vowels
                 current_group = '' + word[i]
+        
+        if current_group_is_vowels:
+            if len(current_group) <= VOWEL_GROUP_MAX_LEN:
+                self.vowel_groups.append(current_group)
+            self.end_groups_vowels.append(current_group)
+        else:
+            if len(current_group) <= CONSONANT_GROUP_MAX_LEN:
+                self.consonant_groups.append(current_group)
+            self.end_groups_consonants.append(current_group)
 
     def train(self, words):
         self.vowel_groups = []
         self.consonant_groups = []
         self.start_groups_vowels = []
         self.start_groups_consonants = []
+        self.end_groups_vowels = []
+        self.end_groups_consonants = []
         self.bigram_map = {}
 
         for word in words:
             self._get_letter_groupings_and_bigrams(word)
 
 
-def get_english_words():
-    english_dict = open('words.txt')
+def get_english_words(dict_filename):
+    english_dict = open(dict_filename, 'r')
     words = set()
     for word in english_dict.readlines():
         word = word.strip()
@@ -76,30 +94,58 @@ def get_english_words():
     english_dict.close()
     return words
 
-def get_name_component(model, english_words, num_groups, start_with_vowel):
-    component = sample(english_words, 1)[0]
-    while component in english_words:
-        component = model.get_start_group(start_with_vowel)
-        current_group_is_vowels = not start_with_vowel
-        for i in range(1, num_groups):
-            component += model.get_group_following(component[-1])
-            current_group_is_vowels = not current_group_is_vowels
+def get_num_letter_groups(mean_groups):
+    groups = int(floor(normalvariate(mean_groups, 1.5)))
+    return 3 if groups < 3 else 7 if groups > 7 else groups
+
+def get_name_component(model, english_words, mean_groups, start_with_vowel):
+    num_groups = get_num_letter_groups(mean_groups)
+    component = model.get_start_group(start_with_vowel)
+    current_group_is_vowels = not start_with_vowel
+    for i in range(1, num_groups - 1):
+        component += model.get_group_following(component[-1])
+        current_group_is_vowels = not current_group_is_vowels
+    component += model.get_end_group_following(component[-1])
     return component.capitalize()
 
-def get_name(model, english_words):
-    first = get_name_component(model, english_words, 4, choice([True, False, False]))
-    last = get_name_component(model, english_words, 4, choice([True, False, False]))
-    return first + ' ' + last
+def get_title(english_words):
+    identifiers = [
+        'King of the',
+        'Protector of the',
+        'Knight of the',
+        'Lord of the',
+        'Master of the',
+        'Defender of the',
+        'Prince of the',
+        'Captain of the',
+        'Commander of the',
+        'King under the',
+        'King beyond the',
+        'King in the',
+    ]
+    words = sample(english_words, 2)
+    return '{identifier} {word1} {word2}'.format(
+        identifier=choice(identifiers),
+        word1=words[0].capitalize(),
+        word2=words[1].capitalize(),
+    )
+
+def get_name(model, english_words, use_titles):
+    first = get_name_component(model, english_words, 4.25, random() < 0.45)
+    last = get_name_component(model, english_words, 5, choice([True, False]))
+    full_name = first + ' ' + last
+    if use_titles:
+        full_name += ', ' + get_title(english_words)
+    return full_name
 
 if __name__ == '__main__':
     names_to_generate = 10
-    english_words = get_english_words()
+    use_titles = True
+    dict_size = 'small'
+
+    english_words = get_english_words(dict_size + '.txt')
     model = NameGeneratorModel()
     model.train(english_words)
 
     for i in range(names_to_generate):
-        print get_name(model, english_words)
-
-
-
-
+        print get_name(model, english_words, use_titles)
